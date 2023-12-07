5df2425a5b8f902515ee46634032a594e1db369b
richdede <deeprince2002@gmail.com>
2023-12-07 12:09:21 -0800

    updated

---
+++ src/app/constants/index.ts
@@ -0,0 +1,47 @@
+export const inclusions = [
+  {
+    title: 'Free Shipping',
+    description: 'Free shipping for order above $150',
+    icon: '/public/assets/icons/shipping.svg',
+  },
+  {
+    title: 'Money Guarantee',
+    description: 'Within 30 days for an exchange',
+    icon: '/public/assets/icons/dollar.svg',
+  },
+  {
+    title: 'Online Support',
+    description: '24 hours a day, 7 days a week',
+    icon: '/public/assets/icons/support.svg',
+  },
+  {
+    title: 'Flexible Payment',
+    description: 'Pay with multiple credit cards',
+    icon: '/public/assets/icons/payment.svg',
+  },
+]
+
+export const profileNavItems = [
+  {
+    title: 'Personal Information',
+    url: '/account',
+    icon: '/public/assets/icons/user.svg',
+  },
+  {
+    title: 'My Purchases',
+    url: '/account/purchases',
+    icon: '/public/assets/icons/purchases.svg',
+  },
+  {
+    title: 'My Orders',
+    url: '/account/orders',
+    icon: '/public/assets/icons/orders.svg',
+  },
+  {
+    title: 'Logout',
+    url: '/logout',
+    icon: '/public/assets/icons/logout.svg',
+  },
+]
+
+export const noHeaderFooterUrls = ['/create-account', '/login', '/recover-password']
